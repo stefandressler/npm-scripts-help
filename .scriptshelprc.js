1fb09fd879731d285e12f7601b3ffb72e643531b
Stefan Dressler <stefan@cliXimac.local>
2016-12-17 10:46:38 +0100

    ascii art & auto-ignore scripts if no help defined

---
+++ .scriptshelprc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "help-message": [
+    "Some explanation that will show on the top.",
+    "It can be multiple lines if you use an array",
+    "or a single line if you use a string"
+  ],
+
+  "help": "Will show example",
+};
--- index.js
+++ index.js
@@ -1,72 +1,95 @@
 var chalk = require('chalk');
 
-var packageJson =require(process.cwd() + '/package.json');
-var scripts = packageJson.scripts || {};
-var scriptsHelpConfig = getScriptsHelpConfig();
-var search = specificScript = process.argv[2];
+const packageJson = require(process.cwd() + '/package.json');
+const scripts = packageJson.scripts || {};
+const scriptsHelpConfig = getScriptsHelpConfig();
+const search = specificScript = process.argv[2];
 
-if (scriptsHelpConfig['help-message']){
-    console.log(chalk.bold.cyan('----------------------------------------------'));
-    console.log(getDesc(scriptsHelpConfig['help-message']));
-    console.log(chalk.bold.cyan('----------------------------------------------'));
+if (scriptsHelpConfig['help-message']) {
+  console.log('                                             .__        __          ');
+  console.log('   ____ ______   _____     ______ ___________|__|______/  |_  ______');
+  console.log('  /    \\\\____ \\ /     \\   /  ___// ___\\_  __ \\  \\____ \\   __\\/  ___/');
+  console.log(' |   |  \\  |_> >  Y Y  \\  \\___ \\\\  \\___|  | \\/  |  |_> >  |  \\___ \\ ');
+  console.log(' |___|  /   __/|__|_|  / /____  >\\___  >__|  |__|   __/|__| /____  >');
+  console.log('      \\/|__|         \\/       \\/     \\/         |__|             \\/ ');
+  console.log(' ');
+  console.log(`${packageJson.name} ${packageJson.version} (node ${process.version}-generic i686)`);
+  console.log('Copyright (c) 2016 Open Source Community and/or its affiliates. Some rights reserved.');
+  console.log('=====================================================================================');
+  console.log(' ');
+  console.log(getDesc(scriptsHelpConfig['help-message']));
+  console.log(' ');
 }
+
 Object.keys(scripts)
-    .filter(function(scriptName){
-        if (!search) return true;
-        return new RegExp(search).test(scriptName)
+  .filter(function (scriptName) {
+    if (!search) return true;
+    return new RegExp(search).test(scriptName)
+  })
+  .map(getScriptHelp)
+  .filter(sh => sh !== undefined)
+  .map(printScriptHelp)
+;
+
+function getScriptHelp(scriptName) {
+  var script = scripts[scriptName] || '';
+  var currentScriptHelp = scriptsHelpConfig[scriptName];
+
+  if (typeof currentScriptHelp === 'string' || Array.isArray(currentScriptHelp)) {
+    currentScriptHelp = {
+      "Description": getDesc(currentScriptHelp)
+    }
+  }
+
+  if (currentScriptHelp !== undefined) {
+    return Object.assign({}, {
+      name: scriptName,
+      Script: script
+    }, currentScriptHelp);
+  }
+}
+
+function printScriptHelp(help) {
+  console.log(chalk.gray('npm run ') + chalk.cyan(help.name));
+
+  Object.keys(help).filter(function (key) {
+    return key !== 'name' && key !== 'Script';
+  })
+    .forEach(function (helpKey) {
+      var desc = getDesc(help[helpKey]);
+      if (helpKey === 'Script') {
+        console.log(chalk.gray(desc));
+      } else {
+        console.log(desc);
+      }
     })
-    .map(getScriptHelp)
-    .map(printScriptHelp)
-
-
-function getScriptHelp (scriptName){
-    var script = scripts[scriptName] || '';
-    var currentScriptHelp = scriptsHelpConfig[scriptName];
-    if (typeof currentScriptHelp === 'string' || Array.isArray(currentScriptHelp)) {
-        currentScriptHelp = {
-            "Description":getDesc(currentScriptHelp)
-        }
-    }
-    return Object.assign({},{
-        name: scriptName,
-        script:script
-    },currentScriptHelp);
+    ;
 }
 
-function printScriptHelp(help){
-    console.log(' ');
-    console.log(chalk.cyan(help.name + ':'));
-    Object.keys(help).filter(function(key){return key !== 'name';})
-        .forEach(function(helpKey){
-            var desc = getDesc(help[helpKey]);
-            console.log(chalk.magenta(helpKey + ': ') + desc);
-    });
-}
+function getDesc(line) {
+  if (!line) return '';
 
-function getDesc(line){
-    if (!line) return '';
+  if (typeof line === 'string') {
+    return line + '\n';
+  }
 
-    if (typeof line === 'string') {
-        return line;
-    }
-
-    if (Array.isArray(line) && line.length > 0 && typeof line[0] === 'string'){
-        return formatMultiLine(line);
-    }
-    return '';
+  if (Array.isArray(line) && line.length > 0 && typeof line[0] === 'string') {
+    return formatMultiLine(line);
+  }
+  return '';
 }
 
 
-function formatMultiLine(multiline){
-    return '\n' +multiline.join('\n');
+function formatMultiLine(multiline) {
+  return multiline.join('\n') + '\n';
 }
 
-function getScriptsHelpConfig(){
-    try{
-        return require(process.cwd() + '/.scriptshelprc.js');
-    }
-    catch (err) {
-        return packageJson['scriptshelp'] || {};
-    }
+function getScriptsHelpConfig() {
+  try {
+    return require(process.cwd() + '/.scriptshelprc.js');
+  }
+  catch (err) {
+    return packageJson['scriptshelp'] || {};
+  }
 
 }
